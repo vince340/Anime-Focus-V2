2eb2b2c9c4e52912e8d5bbe4fb643b465b0c29c9
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2025-02-27 13:41:31 +0530

    Update Banner.jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -120,7 +120,7 @@ function Banner({ item, index }) {
           </button>
           <Link
             to={`/${item.id}`}
-            className="flex bg-[#3B3A52] justify-center items-center px-4 py-2 rounded-3xl gap-x-2 max-[320px]:w-fit max-[320px]:px-3"
+            className="flex bg-[#242328] justify-center items-center px-4 py-2 rounded-3xl gap-x-2 max-[320px]:w-fit max-[320px]:px-3"
           >
             <p className="text-white max-[1000px]:text-[15px] font-semibold max-[320px]:text-[12px]">
               Detail
