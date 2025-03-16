6c8f77914afc5a3e597d9d9c1be3f5548e215559
vince340 <154840844+vince340@users.noreply.github.com>
2025-03-15 20:09:34 -0400

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -60,7 +60,7 @@ function Navbar() {
               onClick={handleHamburgerClick}
             />
               <img
-          src="https://i.postimg.cc/DzSS0VgW/20250227-181745.png"
+          src="https://i.postimg.cc/26Tn7j2g/Anime-Focus.png"
           alt={logoTitle}
           className="w-[160px] h-[39px]"
         />
