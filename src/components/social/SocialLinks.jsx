31c1a851ffad74567778551328367e7309610e47
vince340 <154840844+vince340@users.noreply.github.com>
2025-03-19 22:19:21 -0400

    Update SocialLinks.jsx

--- src/components/social/SocialLinks.jsx
+++ src/components/social/SocialLinks.jsx
@@ -21,7 +21,7 @@ const socialLinks = [
   },
   {
     icon: FaDiscord,
-    href: "https://discord.gg/your-server", // Replace with your Discord server invite link
+    href: "https://discord.gg/BesnYbSe", // Replace with your Discord server invite link
     color: "#7289DA",
     label: "Discord"
   }
